005eb3b2362470ce6daa146a133139fe1b0e60f9
Hull Tiffany C <tiffhull661@gmail.com>
2019-04-17 10:23:11 -0400

    Finished Tile(card) Match - Added fully functioning card flipping (front and back) - Matches disappear , non-matches flip back over - Added timer - Win/lose messages - Randomized card positions

--- TreasureBound/TreasureBound/CardCollectionViewCell.swift
+++ TreasureBound/TreasureBound/CardCollectionViewCell.swift
@@ -19,19 +19,72 @@ class CardCollectionViewCell: UICollectionViewCell {
     
     func setCard(_ card:Card) {
         
-        //keeps track of card that gets passed in
+        //Keeps track of card that gets passed in
         self.card = card
         
+        if card.isMatched == true {
+            //If card has been matched, make img views invisible
+            backImgView.alpha = 0
+            frontImgView.alpha = 0
+            
+            return
+        }
+        else {
+            //If card hasn't been matched, keep img views visible
+            backImgView.alpha = 1
+            frontImgView.alpha = 1
+        }
+        
         frontImgView.image = UIImage(named: card.imgName)
+        
+        //Finds out if card is flipped face up or face down
+        if card.isFlipped == true {
+            UIView.transition(from: backImgView,
+                              to: frontImgView,
+                              duration: 0,
+                              options: [.transitionFlipFromLeft, .showHideTransitionViews],
+                              completion: nil)
+            
+        }
+        else {
+            UIView.transition(from: frontImgView,
+                              to: backImgView,
+                              duration: 0,
+                              options: [.transitionFlipFromLeft, .showHideTransitionViews],
+                              completion: nil)
+        }
     }
     
+    //Flips card from back to front
     func flip() {
         
-        UIView.transition(from: backImgView, to: frontImgView, duration: 0.3, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        UIView.transition(from: backImgView,
+                          to: frontImgView,
+                          duration: 0.3,
+                          options: [.transitionFlipFromLeft, .showHideTransitionViews],
+                          completion: nil)
     }
     
+    //Flips card from front to back
     func flipBack() {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) {
         
-        
+        UIView.transition(from: self.frontImgView,
+                          to: self.backImgView,
+                          duration: 0.3,
+                          options: [.transitionFlipFromRight, .showHideTransitionViews],
+                          completion: nil)
+        }
     }
+    
+    func remove() {
+        //Removes both img views from being visible
+        
+        //Animation
+        UIView.animate(withDuration: 0.4, delay: 0.5,
+                       options: .curveEaseOut,
+                       animations: {self.frontImgView.alpha = 0},
+                       completion: nil)
+    }
+    
 }
--- TreasureBound/TreasureBound/CardModel.swift
+++ TreasureBound/TreasureBound/CardModel.swift
@@ -11,33 +11,48 @@ import Foundation
 class CardModel {
     
     func getCards() -> [Card] {
+        //Array to store numbers that have already generated
+        var generatedNumsArray = [Int]()
+        
         // Declare array to store generated cards
         var generatedCardsArray = [Card]()
         
         //randomly generate pairs of cards
-        for _ in 1...8 {
+        while generatedNumsArray.count < 8 {
             //gets random number
             let randNum = arc4random_uniform(13) + 1
         
-            //logs the number
-            print(randNum)
-            
-            //First card obj
-            let cardOne = Card()
-            cardOne.imgName = "card\(randNum)"
-            
-            generatedCardsArray.append(cardOne)
-            
-            //Second card obj
-            let cardTwo = Card()
-            cardTwo.imgName = "card\(randNum)"
-            
-            generatedCardsArray.append(cardTwo)
-            //unique pairs of cards
+            if generatedNumsArray.contains(Int(randNum)) == false {
+                //logs the number
+                print(randNum)
+                
+                //Stores num into generatedNumsArray
+                generatedNumsArray.append(Int(randNum))
+                
+                //First card obj
+                let cardOne = Card()
+                cardOne.imgName = "card\(randNum)"
+                
+                generatedCardsArray.append(cardOne)
+                
+                //Second card obj
+                let cardTwo = Card()
+                cardTwo.imgName = "card\(randNum)"
+                
+                generatedCardsArray.append(cardTwo)
+            }            
         }
         
-        //todo: randomize array
-        
+        // randomize card array
+        for i in 0...generatedCardsArray.count - 1 {
+            //find randomized card to swap with
+            let randNum = Int(arc4random_uniform(UInt32(generatedCardsArray.count)))
+            
+            //swap the cards
+            let tempStorage = generatedCardsArray[i]
+            generatedCardsArray[i] = generatedCardsArray[randNum]
+            generatedCardsArray[randNum] = tempStorage
+        }
         //return array
         return generatedCardsArray
     }
